3bf1c1391be92a646dfd32972d790a2c9eba5ca0
Mateus Vargas <mat358591@gmail.com>
2022-09-15 10:37:56 -0300

    Create Main.java

---
+++ POO/compra-composicao/Main.java
@@ -0,0 +1,19 @@
+public class Main{
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa("joao", "233", "rua a", "caxias", "rs");
+        Produto prod1 = new Produto(223, "abacaxi");
+        Produto prod2 = new Produto(655, "mamao");
+
+        p1.consultaEndereco();
+        Compra c = new Compra(p1);
+        c.comprar(p1, prod1);
+        c.comprar(p1, prod2);
+        c.verificarCompra();
+
+        Pessoa p2 = new Pessoa("jonas", "233", "rua a", "alegrete", "rs");
+        Compra c2 = new Compra(p2);
+        p2.consultaEndereco();
+        c2.comprar(p2, prod2);
+        c2.verificarCompra();
+    }
+}
