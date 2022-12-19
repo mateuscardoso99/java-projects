2706e86b17fd2efe5f8b0b7e0a35a71dc170ad24
Mateus Cardoso <mat358591@gmail.com>
2022-12-19 16:45:53 -0300

    Update Empregado.java

--- POO/polimorfismo/ex-1/Empregado.java
+++ POO/polimorfismo/ex-1/Empregado.java
@@ -1,1 +1,19 @@
+package polimorfismo;
 
+public class Empregado extends Funcionario{
+    
+    public Empregado(String nome, int cpf, double salario) {
+        super(nome, cpf);
+        this.salario=salario;
+    }
+
+    @Override
+    public void imprimeCargo(){
+        System.out.println("cargo: empregado");
+    }
+    
+    @Override
+    public void imprimeSalario(){
+        System.out.println("salario: "+getSalario());
+    }
+}
