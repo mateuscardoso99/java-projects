2a581bb362c1126a9027f07d7ba3bf466cf6c3bf
Mateus Cardoso <mat358591@gmail.com>
2023-01-02 10:59:30 -0300

    Update generic-methods_curingas.java

--- generic-methods_curingas.java
+++ generic-methods_curingas.java
@@ -73,6 +73,38 @@ class Teste
 
 Curingas:
 
+
+Caracteres curinga de limite superior: Esses curingas podem ser usados quando você deseja relaxar as restrições de uma variável. 
+Por exemplo, digamos que você queira escrever um método que funcione em List <integer>, List <double> e List <number>, 
+você pode fazer isso usando um curinga de limite superior.
+Para declarar um curinga de limite superior, use o caractere curinga ('?'), Seguido pela palavra-chave extends, seguido por seu limite superior.
+public static void add (List <? extends Number> list)  //qualquer classe que seja filha de Number
+
+
+
+Curingas de limite inferior: é expresso usando o caractere curinga ('?'), Seguido pela palavra-chave super, seguida por seu limite inferior: <? super A>.
+Sintaxe: Collectiontype <? super A>  //qualquer clase que seja mãe de A
+por exemplo se passar Integer em A os argumentos podem ser Inteiro ou superclasse de Inteiro (que é Number)
+
+
+
+Curinga ilimitado: este tipo de curinga é especificado usando o caractere curinga (?), Por exemplo, Lista. Isso é chamado de lista de tipo desconhecido. E
+les são úteis nos seguintes casos
+Ao escrever um método que pode ser empregado usando a funcionalidade fornecida na classe Object.
+Quando o código está usando métodos na classe genérica que não dependem do parâmetro de tipo
+
+ex:
+List<?> curinga = null;
+List<String> string = Arrays.asList("Andrei", "skqo");
+List<Integer> inteiros = Arrays.asList(1, 2);
+
+curinga = string; // SEM PROBLEMAS
+curinga = inteiros; // SEM PROBLEMAS
+    
+    
+////////////////////
+
+
 Suponha que se queira implementar um método genérico
 sum que some os números de uma coleção
 (ArrayList).
@@ -117,7 +149,7 @@ public class Main
     especifica um nome de parâmetro de tipo, você não pode
     utilizá-lo como um nome de tipo por todo o corpo do
     método (isto é, não pode substituir Number por ? na
-    linha 95).
+    linha 23).
     Você pode, porém, declarar o método sum dessa maneira:
     public static<T extends Number> doublesum (ArrayList< T > list)
     Isso permite ao método receber uma ArrayList que
@@ -125,5 +157,4 @@ public class Main
 	*/
 	
 }
-
 
