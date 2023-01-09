673e38f3f0e803d618b3622d2c10dedb17204aa9
Mateus Cardoso <mat358591@gmail.com>
2023-01-09 09:21:58 -0300

    Update Main.java

--- POO/heranca/Main.java
+++ POO/heranca/Main.java
@@ -1,3 +1,8 @@
+//OBS: só é possível acessar o método da classe mãe, classes que estão acima na hierarquia não é possível acessar seus métodos
+//ex: classe Object tem o método toString, classe A herda de Object e sobrescreve o toString, classe B herda de A e sobrescreve o toString
+//nesse caso a classe B só consegue acessar o toString da classe A, o da classe Object ela não acessa, somente a classe A pode acessar o toString
+//da classe Object ou seja é sempre no máximo 1 nível acima
+
 public class Main{
     public static void main(String[] args) {
         Gerente g1 = new Gerente();
