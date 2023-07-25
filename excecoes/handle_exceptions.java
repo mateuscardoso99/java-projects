e43baa9ac644e03cfdcafa4749d1a8fd943450cb
Mateus Cardoso <mat358591@gmail.com>
2023-07-25 11:49:53 -0300

    Update handle_exceptions.java

--- excecoes/handle_exceptions.java
+++ excecoes/handle_exceptions.java
@@ -1,3 +1,10 @@
+/*
+Todas as classes de exceção são subtipos da classe java.lang.Exception. 
+A classe Exception é uma subclasse da classe Throwable. Além da classe Exception, há outra subclasse chamada Error, que é derivada da classe Throwable. 
+Erros são condições anormais que ocorrem em caso de falhas graves, não são tratadas pelos programas Java. Erros são gerados para indicar erros gerados pelo ambiente de tempo de execução. 
+Exemplo: JVM está sem memória. Normalmente, os programas não podem se recuperar de erros. A classe Exception tem duas subclasses principais: classe IOException e classe RuntimeException.
+*/
+
 //ex: receber exceção lançada no método que foi chamado
 //Exceções verificadas: Uma exceção verificada é uma exceção que é verificada (notificada) pelo compilador em tempo de compilação, também chamada de exceções de tempo de compilação. Essas exceções não podem ser simplesmente ignoradas, o programador deve cuidar (tratar) dessas exceções
 //Exceções não verificadas: Uma exceção não verificada é uma exceção que ocorre no momento da execução. Eles também são chamados de exceções de tempo de execução. Isso inclui bugs de programação, como erros de lógica ou uso indevido de uma API. As exceções de tempo de execução são ignoradas no momento da compilação
