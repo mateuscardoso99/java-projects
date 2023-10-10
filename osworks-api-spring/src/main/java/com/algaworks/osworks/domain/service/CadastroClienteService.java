349b256bf6dcf2a18646122dd81880de47ac5afa
Mateus Cardoso <mat358591@gmail.com>
2023-10-10 10:49:09 -0300

    Create CadastroClienteService.java

---
+++ osworks-api-spring/src/main/java/com/algaworks/osworks/domain/service/CadastroClienteService.java
@@ -0,0 +1,30 @@
+package com.algaworks.osworks.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.algaworks.osworks.domain.exception.NegocioException;
+import com.algaworks.osworks.domain.model.Cliente;
+import com.algaworks.osworks.domain.repository.ClienteRepository;
+
+@Service
+public class CadastroClienteService {
+
+	//classe para manipular regras de negócio
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	public Cliente salvar(Cliente cliente) {
+		Cliente clienteExistente = clienteRepository.findByEmail(cliente.getEmail());
+		if(clienteExistente != null && !clienteExistente.equals(cliente)) {
+			throw new NegocioException("já existe um cliente com esse email");
+		}
+		
+		return clienteRepository.save(cliente);
+	}
+	
+	public void excluir(Long id) {
+		clienteRepository.deleteById(id);
+	}
+}
