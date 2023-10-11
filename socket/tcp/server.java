d782cada0d806f8e53ea7678de6be0c0bbc328f5
Mateus Cardoso <mat358591@gmail.com>
2023-10-11 09:03:57 -0300

    Update server.java

--- socket/tcp/server.java
+++ socket/tcp/server.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/*
+ServerSocket é um ouvinte que aguarda o estabelecimento de conexões de soquete. 
+Socket é o canal de comunicação entre dois sistemas (um servidor e outro cliente)
+*/
 class Server{
     public static void main(String[] args) throws IOException{
         ServerSocket serverSocket = new ServerSocket(8081);
