70500fb39d820e479ca0b1fee1fc7d40d3b4e1a4
Mateus Cardoso <mat358591@gmail.com>
2023-10-16 18:36:36 -0300

    Update server.java

--- socket/tcp/server.java
+++ socket/tcp/server.java
@@ -7,6 +7,10 @@ import java.net.Socket;
 /*
 ServerSocket é um ouvinte que aguarda o estabelecimento de conexões de soquete. 
 Socket é o canal de comunicação entre dois sistemas (um servidor e outro cliente)
+quando um pacote é recebido ou enviado é criada uma nova conexão (novo socket) com o remetente ou destinatario 
+dessa forma os envios e recebimentos seguintes serão sempre por esse socket
+um socket possui IP de origem e destino e porta de origem e destino
+ao contrário do UDP em que existe um socket apenas que recebe pacotes de todos no TCP existe um socket pra cada conexão
 */
 class Server{
     public static void main(String[] args) throws IOException{
