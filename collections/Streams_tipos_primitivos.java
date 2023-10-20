860570543fa5b7fe6112b256612e0800185b8c7e
Mateus Cardoso <mat358591@gmail.com>
2023-10-20 11:32:27 -0300

    Update Streams_tipos_primitivos.java

--- collections/Streams_tipos_primitivos.java
+++ collections/Streams_tipos_primitivos.java
@@ -5,7 +5,12 @@ class Teste{
         String[] s = {"teste"};
         int[] i = {9,8,6,0,2};
 
-        Arrays.stream(s).forEach(System.out::println);
+        Arrays.stream(s).forEach(System.out::println);//Arrays.stream() funciona apenas para arrays primitivos do tipo int[], long[] e double[] e retorna IntStream, LongStream e DoubleStream
         Arrays.stream(i).filter(v->v%2==0).forEach(System.out::println);
+
+        //Stream.of
+        int arr[] = { 1, 2, 3, 4, 5 };
+	    Stream<int[]> stream = Stream.of(arr);//Passando um array inteiro, o método Stream.of() retorna Stream enquanto Arrays.stream() retorna um IntStream
+		stream.flatMapToInt(Arrays::stream).forEach(System.out::println);
     }
 }
