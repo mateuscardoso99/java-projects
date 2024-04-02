1697d54a6f340d65c156a2abc255aa18a4958dba
Mateus Cardoso <mat358591@gmail.com>
2024-04-02 18:01:29 -0300

    Update Main.java

--- padroes_projeto/ex-7_json_from_class_attrs/src/main/java/org/example/Main.java
+++ padroes_projeto/ex-7_json_from_class_attrs/src/main/java/org/example/Main.java
@@ -17,6 +17,9 @@ public class Main {
         teste.setNome("joao");
         teste.setIdade(19);
         teste.setEndereco("rua das couves");
+        teste.setIdColuna(Double.valueOf(234));
+        teste.setNumeros(new Integer[]{1,2,5,9,6});
+        teste.setNumeros2(List.of(57,23,656,32));
 
         new Main().convert(teste);        
     }
@@ -35,13 +38,47 @@ public class Main {
                 System.out.println(a.toString());
             }*/
 
-            if(Arrays.stream(anotacoesDoMetodo).anyMatch(a -> a.toString().endsWith("JSON()")) && m.getName().startsWith("get")){
-                String nomePropriedade = m.getName().substring(3).toLowerCase();
+            if(Arrays.stream(anotacoesDoMetodo).anyMatch(a -> a.annotationType().equals(JSON.class)) && m.getName().startsWith("get")){
+                String nomePropriedade = m.getName().substring(3);
+                
+                nomePropriedade = Character.toLowerCase(nomePropriedade.charAt(0)) + nomePropriedade.substring(1);
+
                 Object valorPropriedade = m.invoke(object);
 
                 json.append("\"" + nomePropriedade + "\"");
                 json.append(":");
-                json.append("\"" + valorPropriedade + "\",");
+
+                Class<?> retornoMetodo = m.getReturnType();
+
+                if(retornoMetodo.isPrimitive()){
+                    json.append(valorPropriedade);
+                    json.append(",");
+                }
+                else if(retornoMetodo.isArray()){
+                    json.append('[');
+
+                    int length = Array.getLength(valorPropriedade);
+
+                    for(int i=0; i<length; i++){
+                        json.append(Array.get(valorPropriedade, i));
+                        json.append(",");
+                    }
+
+                    json.append(']');
+                }
+                else if(Collection.class.isAssignableFrom(retornoMetodo)){
+                    json.append('[');
+
+                    json.append(']');
+                }
+                else{
+                    json.append("\"" + valorPropriedade + "\",");
+                }
+
+                //System.out.println(m.getName() + " " + m.getReturnType().isPrimitive());
+                //System.out.println(m.getName() + " " + m.getReturnType().isArray());
+                //System.out.println(Collection.class.isAssignableFrom(m.getReturnType()));
+
             }
 
         }
