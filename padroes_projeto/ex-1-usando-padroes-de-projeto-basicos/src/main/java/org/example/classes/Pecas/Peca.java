f0cc85cbb5b08635f454a2941c92a8f76ef3ee4d
Mateus Cardoso <mat358591@gmail.com>
2024-04-16 16:35:32 -0300

    Create Peca.java

---
+++ padroes_projeto/ex-1-usando-padroes-de-projeto-basicos/src/main/java/org/example/classes/Pecas/Peca.java
@@ -0,0 +1,63 @@
+package org.example.classes.Pecas;
+
+//padrão de projeto: Abstract Parent class
+public abstract class Peca {
+    protected Integer codigo;
+    protected Double area;
+    
+    public Peca(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public abstract void calcularArea();
+
+    //padrão de projeto: Accessor Methods
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public Double getArea() {
+        return area;
+    }
+
+    public void setArea(Double area) {
+        this.area = area;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+        result = prime * result + ((area == null) ? 0 : area.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Peca other = (Peca) obj;
+        if (codigo == null) {
+            if (other.codigo != null)
+                return false;
+        } else if (!codigo.equals(other.codigo))
+            return false;
+        if (area == null) {
+            if (other.area != null)
+                return false;
+        } else if (!area.equals(other.area))
+            return false;
+        return true;
+    }
+    
+    
+}
