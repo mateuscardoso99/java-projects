592e81b894e046cc1ca6d3a4cc858e36de785ca4
Mateus Cardoso <mat358591@gmail.com>
2024-04-26 10:28:58 -0300

    Add files via upload

---
+++ padroes_projeto/abstract-factory/MacOSButton.java
@@ -0,0 +1,8 @@
+public class MacOSButton implements Button{
+
+    @Override
+    public void paint() {
+        System.out.println("botão pra MacOS criado");
+    }
+    
+}
---
+++ padroes_projeto/abstract-factory/MacOSCheckbox.java
@@ -0,0 +1,8 @@
+public class MacOSCheckbox implements Checkbox{
+
+    @Override
+    public void paint() {
+        System.out.println("checkbox pra MacOS criado");
+    }
+    
+}
