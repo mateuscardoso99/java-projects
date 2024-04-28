0d24561c6cac91dee82767e161ddd26f2c255bc1
Mateus Cardoso <mat358591@gmail.com>
2024-04-28 10:30:58 -0300

    Update Application.java

--- padroes_projeto/abstract-factory/Application.java
+++ padroes_projeto/abstract-factory/Application.java
@@ -1,3 +1,7 @@
+// O código do cliente funciona apenas com fábricas e produtos 
+// através de tipos abstratos: GUIFactory, Button e Checkbox. isso 
+// permite passar qualquer fábrica ou subclasse de produto para o cliente 
+// sem quebrar o código
 public class Application {
     private Button button;
     private Checkbox checkbox;
