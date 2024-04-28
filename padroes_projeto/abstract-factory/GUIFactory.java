d7bc398e370111335fc1f4a083b67711bec32908
Mateus Cardoso <mat358591@gmail.com>
2024-04-28 10:32:41 -0300

    Update GUIFactory.java

--- padroes_projeto/abstract-factory/GUIFactory.java
+++ padroes_projeto/abstract-factory/GUIFactory.java
@@ -1,3 +1,10 @@
+// A interface abstrata de fábrica declara um conjunto de métodos que
+// retorna diferentes produtos abstratos. Esses produtos são chamados
+// uma família e estão relacionados por um tema ou conceito de alto nível.
+// Produtos de uma família geralmente conseguem colaborar entre
+// eles mesmos. Uma família de produtos pode ter diversas variantes,
+// mas os produtos de uma variante são incompatíveis com o
+// produtos de outra variante.
 public interface GUIFactory {
     Button createButton();
     Checkbox createCheckbox();
