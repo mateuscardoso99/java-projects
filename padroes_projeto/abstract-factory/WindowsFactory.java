c9a74de6f4d627c518762135175a471119a09297
Mateus Cardoso <mat358591@gmail.com>
2024-04-28 10:32:20 -0300

    Update WindowsFactory.java

--- padroes_projeto/abstract-factory/WindowsFactory.java
+++ padroes_projeto/abstract-factory/WindowsFactory.java
@@ -1,3 +1,8 @@
+//As fábricas concretas produzem uma família de produtos que pertencem
+//a uma única variante. A fábrica garante que o
+// os produtos resultantes são compatíveis. Assinaturas do
+// métodos concreto da fábrica retornam um produto abstrato, enquanto dentro
+// o método de um produto concreto é instanciado
 public class WindowsFactory implements GUIFactory{
 
     @Override
