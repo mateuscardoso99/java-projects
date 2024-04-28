d477da8826bbfac90b91163aaada03a7432b52cc
Mateus Cardoso <mat358591@gmail.com>
2024-04-28 10:33:41 -0300

    Update Button.java

--- padroes_projeto/abstract-factory/Button.java
+++ padroes_projeto/abstract-factory/Button.java
@@ -1,3 +1,5 @@
+// Cada produto distinto de uma família de produtos deve ter uma interface base 
+// Todas as variantes do produto devem implementar esta interface.
 public interface Button {
     void paint();
 }
