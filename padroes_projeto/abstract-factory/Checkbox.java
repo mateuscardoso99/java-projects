51b29ca9c37b588c67757211aa705126b89dcdd0
Mateus Cardoso <mat358591@gmail.com>
2024-04-28 10:33:20 -0300

    Update Checkbox.java

--- padroes_projeto/abstract-factory/Checkbox.java
+++ padroes_projeto/abstract-factory/Checkbox.java
@@ -1,3 +1,6 @@
+// Aqui está a interface base de outro produto. Todos os produtos 
+// podem interagir entre si, mas a interação adequada é 
+// possível apenas entre produtos da mesma variante concreta.
 public interface Checkbox {
     void paint();
 }
