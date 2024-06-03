e092fa46eb66de54d99cddea73d2db04f7375d82
Mateus Cardoso <mat358591@gmail.com>
2024-06-03 11:54:26 -0300

    Update ContaBancaria.java

--- padroes_projeto/padroes-projeto--versao-professor/src/main/java/br/ufsm/csi/pp/exercicio2/ContaBancaria.java
+++ padroes_projeto/padroes-projeto--versao-professor/src/main/java/br/ufsm/csi/pp/exercicio2/ContaBancaria.java
@@ -1,5 +1,6 @@
 package br.ufsm.csi.pp.exercicio2;
 
+import br.ufsm.csi.pp.exercicio3a.Prototype;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,6 +17,22 @@ public abstract class ContaBancaria {
     private String numero;
     private Double saldo;
     private List<Movimentacao> movimentacoes = new LinkedList<>();
+
+    public enum TipoContaBancaria { CONTA_CORRENTE, POUPANCA, FUNDOS_RENDA }
+    public static ContaBancaria newInstance(TipoContaBancaria tipoContaBancaria) {
+        switch (tipoContaBancaria) {
+            case POUPANCA -> {
+                return Poupanca.newInstance();
+            }
+            case FUNDOS_RENDA -> {
+                return FundosRenda.newInstance();
+            }
+            case CONTA_CORRENTE -> {
+                return ContaCorrente.newInstance();
+            }
+        }
+        return null;
+    }
 
     public abstract Double calculaIR();
 
