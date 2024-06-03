871ad33c663f16068af17c638c851731db6f3dfa
Mateus Cardoso <mat358591@gmail.com>
2024-06-03 11:46:12 -0300

    Add files via upload

---
+++ padroes_projeto/padroes-projeto--versao-professor/src/main/java/br/ufsm/csi/pp/exercicio3a/ContaBancaria.java
@@ -0,0 +1,8 @@
+package br.ufsm.csi.pp.exercicio3a;
+
+public class ContaBancaria implements Prototype<ContaBancaria> {
+    @Override
+    public ContaBancaria clone() {
+        return this.clone();
+    }
+}
