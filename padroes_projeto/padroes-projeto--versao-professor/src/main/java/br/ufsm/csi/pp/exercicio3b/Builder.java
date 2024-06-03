cac41b551716236aeb435820b072d237ecb38fc5
Mateus Cardoso <mat358591@gmail.com>
2024-06-03 11:46:52 -0300

    Create Builder.java

---
+++ padroes_projeto/padroes-projeto--versao-professor/src/main/java/br/ufsm/csi/pp/exercicio3b/Builder.java
@@ -0,0 +1,22 @@
+package br.ufsm.csi.pp.exercicio3b;
+
+public interface Builder {
+
+    void fazMassa();
+    void colocaMolhoVermelho();
+
+    void colocaQueijoMussarela();
+
+    void colocaQueijoParmesao();
+
+    void colocaQueijoCheddar();
+
+    void fazBordaRecheada();
+
+    void colocaCalabreza();
+
+    void colocarFile();
+
+    void colocaCebola();
+
+}
