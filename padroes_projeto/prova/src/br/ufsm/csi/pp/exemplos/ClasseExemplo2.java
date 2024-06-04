4dc40d011d8bd7e892f15b7c4dc1690328798989
Mateus Cardoso <mat358591@gmail.com>
2024-06-04 16:35:31 -0300

    Add files via upload

---
+++ padroes_projeto/prova/src/br/ufsm/csi/pp/exemplos/ClasseExemplo2.java
@@ -0,0 +1,27 @@
+package br.ufsm.csi.pp.exemplos;
+
+import br.ufsm.csi.pp.annotations.DeepCopy;
+
+public class ClasseExemplo2 {
+
+    private Long codigo;
+    private String nome;
+    @DeepCopy
+    public ClasseExemplo3 exemplo3;
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
