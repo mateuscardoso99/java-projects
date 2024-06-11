76c720437a1b4526a1b4737aa2c37b45224110cb
Mateus Cardoso <mat358591@gmail.com>
2024-06-11 16:51:54 -0300

    Create Banco.java

---
+++ padroes_projeto/decorator/src/main/java/org/example/Banco.java
@@ -0,0 +1,111 @@
+package org.example;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Banco implements BancoInterface {
+
+    private Map<String, ContaBancaria> contas = new ConcurrentHashMap<>();
+
+    private Banco() {}
+
+    private static BancoInterface INSTANCE;
+
+        public static BancoInterface getInstance() {
+        synchronized (Banco.class) {
+            if (INSTANCE == null) {
+                INSTANCE = new LogBancoDecorator(new Banco()); //em vez de instancias banco diretamento, usa o decorator e o decorator instancia banco
+            }
+        }
+        return INSTANCE;
+    }
+
+    public Map<String, ContaBancaria> getContas() {
+        return contas;
+    }
+
+    @Override
+    public boolean excluiConta(String numero) {
+        return contas.remove(numero) != null;
+    }
+
+    @Override
+    public void saque(String numero, Double valor) {
+        ContaBancaria contaBancaria = contas.get(numero);
+        if (contaBancaria != null) {
+            contaBancaria.saque(valor);
+        }
+    }
+
+    @Override
+    public void deposito(String numero, Double valor) {
+        if (valor <= 0) {
+            throw new IllegalArgumentException("valor nao permitido para deposito.");
+        }
+        ContaBancaria contaBancaria = contas.get(numero);
+        if (contaBancaria == null) {
+            throw new IllegalArgumentException("conta nao existente.");
+        }
+        contaBancaria.deposito(valor);
+    }
+
+    @Override
+    public Double saldo(String numero) {
+        ContaBancaria contaBancaria = contas.get(numero);
+        if (contaBancaria == null) return null;
+        return contaBancaria.getSaldo();
+    }   
+
+    @Override
+    public String extrato(String numero) {
+        ContaBancaria contaBancaria = contas.get(numero);
+        if (contaBancaria == null) return null;
+        return contaBancaria.extrato();
+    }
+
+    @Override
+    public void transferencia(String contaOrigem, String contaDestino, Double valor) {
+        saque(contaOrigem, valor);
+        deposito(contaDestino, valor);
+    }
+
+    @Override
+    public ContaBancaria criaConta(TipoConta tipo, Double saldoInicial, boolean especial, Double limite) {
+        ContaBancaria conta = null;
+        if (tipo == TipoConta.CC) {
+            ContaCorrente cc = ContaCorrente.newInstance();
+            cc.setEspecial(especial);
+            cc.setLimite(limite);
+            cc.setSaldo(saldoInicial);
+            cc.setNumero(criaNumeroConta());
+            conta = cc;
+        } else if (tipo == TipoConta.POUPANCA) {
+            Poupanca poupanca = Poupanca.newInstance();
+            poupanca.setNumero(criaNumeroConta());
+            poupanca.setSaldo(saldoInicial);
+            conta = poupanca;
+        } else if (tipo == TipoConta.RENDA_FIXA || tipo == TipoConta.RENDA_VARIAVEL) {
+            FundosRenda fundosRenda = FundosRenda.newInstance();
+            fundosRenda.setTipo(tipo == TipoConta.RENDA_FIXA ? FundosRenda.TipoFundo.RENDA_FIXA : FundosRenda.TipoFundo.RENDA_VARIAVEL);
+            fundosRenda.setNumero(criaNumeroConta());
+            fundosRenda.setSaldo(saldoInicial);
+            conta = fundosRenda;
+        }
+        if (conta != null) {
+            contas.put(conta.getNumero(), conta);
+        }
+        return null;
+    }
+
+    private Long numConta = 1l;
+    private String criaNumeroConta() {
+        synchronized (this) {
+            return String.valueOf(numConta++);
+        }
+    }
+
+    public enum TipoConta {
+        CC, POUPANCA, RENDA_FIXA, RENDA_VARIAVEL
+    }
+
+}
