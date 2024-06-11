03ff369da3415f54110d177a29044e1350ea0ad4
Mateus Cardoso <mat358591@gmail.com>
2024-06-11 16:59:23 -0300

    Add files via upload

---
+++ padroes_projeto/prova-versao-professor/src/main/java/org/example/ClasseExemplo3.java
@@ -0,0 +1,16 @@
+package br.ufsm.csi.pp.exemplos;
+
+public class ClasseExemplo3 {
+
+    private Long codigo;
+    public ClasseExemplo exemplo;
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+}
