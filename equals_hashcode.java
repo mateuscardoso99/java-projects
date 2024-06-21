5ad179669e89c80f1e474091cca99a5440a359e0
Mateus Cardoso <mat358591@gmail.com>
2024-06-21 17:29:14 -0300

    Update equals_hashcode.java

--- equals_hashcode.java
+++ equals_hashcode.java
@@ -69,6 +69,8 @@ class A{
      * Dois objetos diferentes podem ter o mesmo hashCode, mas isto deve ser evitado
 
      * método contains() de um hashSet se baseia no hashCode pra verificar se um item existe ou não
+
+     *Um algoritmo bem implementado reduz o número de colisões e um algoritmo mal implementado aumenta-o. Por exemplo, você pode substituir hashCode() para que retorne um valor constante. Neste caso, todos os elementos serão armazenados em um balde (reduzindo o desempenho), privando-nos da propriedade mais importante graças à qual o HashMap é tão eficiente.
      */
     @Override
     public int hashCode() {
@@ -78,4 +80,91 @@ class A{
         result = prime * result + idade;
         return result;
     }    
+
+
+
+
+    
+    /*
+    EXEMPLO HASHCODE:
+    */
+    public static class DataKey {
+
+	    private String name;
+	    private int id;
+
+        // getter and setter methods
+
+    	@Override
+    	public String toString() {
+    		return "DataKey [name=" + name + ", id=" + id + "]";
+    	}
+
+    }
+
+    import java.util.HashMap;
+    import java.util.Map;
+    
+    public class HashingTest {
+    
+    	public static void main(String[] args) {
+    		Map<DataKey, Integer> hm = getAllData();
+    
+    		DataKey dk = new DataKey();
+    		dk.setId(1);
+    		dk.setName("Pankaj");
+    		System.out.println(dk.hashCode());
+    
+    		Integer value = hm.get(dk); //null
+    
+    		System.out.println(value);
+    
+    	}
+    
+    	private static Map<DataKey, Integer> getAllData() {
+    		Map<DataKey, Integer> hm = new HashMap<>();
+    
+    		DataKey dk = new DataKey();
+    		dk.setId(1);
+    		dk.setName("Pankaj");
+    		System.out.println(dk.hashCode());
+    
+    		hm.put(dk, 10);
+    
+    		return hm;
+        }
+    }
+
+    //Quando executamos o programa acima, ele será impresso null. É porque o método Object hashCode() é usado para encontrar a "caixa" para procurar a chave. 
+    //Como não temos acesso às chaves HashMap e estamos criando a chave novamente para recuperar os dados, você notará que os valores do código hash de ambos os objetos são diferentes e, 
+    //portanto, o valor não foi encontrado.
+    //OBRIGATÓRIAMENTE PRECISA SOBREESCREVER O EQUALS E HASHCODE AO TRABALHAR COM COLLECTIONS BASEADAS EM HASH, COMO SET E MAP
+
+    /*
+    Em termos muito simples, as implementações de tabela Java Hash usam a seguinte lógica para operações get e put nas collections baseadas em hash.
+
+    Primeiro identifique a “caixa” a ser usado usando o código hash da “chave”.
+    Se não houver objetos presentes na "caixa" com o mesmo código hash, adicione o objeto para operação put e retorne nulo para operação get. (por isso o get da linha 118 retorna null)
+    Se houver outros objetos na “caixa” com o mesmo código hash, o método “chave” equals() entra em ação.
+    Se equals() retornar true e for uma operação put, o valor do objeto será substituído.
+    Se equals() retornar falso e for uma operação put, uma nova entrada será adicionada ao intervalo.
+    Se equals() retornar true e for uma operação get, o valor do objeto será retornado.
+    Se equals() retornar false e for uma operação get, então null será retornado.
+
+    O fenômeno quando duas chaves têm o mesmo código hash é chamado de colisão hash. Se o método hashCode() não for implementado corretamente, haverá um número maior de colisões de hash e as 
+    entradas do mapa não serão distribuídas corretamente, causando lentidão nas operações get e put. Esta é a razão para o uso de números primos na geração de código hash para que as 
+    entradas do mapa sejam distribuídas adequadamente em todos as "caixas".
+
+    JÁ VIMOS ACIMA QUE SE HASHCODE() NÃO FOR IMPLEMENTADO, NÃO SEREMOS CAPAZES DE RECUPERAR O VALOR PORQUE O HASHMAP USA CÓDIGO HASH PARA ENCONTRAR A "CAIXA" PARA PROCURAR A ENTRADA. 
+    SE USARMOS APENAS HASHCODE() E NÃO IMPLEMENTARMOS EQUALS(), ENTÃO O VALOR TAMBÉM NÃO SERÁ RECUPERADO PORQUE O MÉTODO EQUALS() RETORNARÁ FALSO, POIS O EQUALS() DE OBJECT COMPARA ENDEREÇOS DE MEMÓRIA APENAS
+    
+    DICAS:
+        Use as mesmas propriedades nas implementações dos métodos equals() e hashCode(), para que seu contrato não seja violado quando qualquer propriedade for atualizada.
+        
+        É melhor usar objetos imutáveis ​​como chave da tabela Hash para que possamos armazenar em cache o código hash em vez de calculá-lo em cada chamada. 
+        É por isso que String é um bom candidato para chave de tabela Hash porque é imutável e armazena em cache o valor do código hash.
+        
+        Implemente o método hashCode() para que ocorra o menor número de colisões de hash e as entradas sejam distribuídas uniformemente em todos as "caixas".
+    */
+    
 }
