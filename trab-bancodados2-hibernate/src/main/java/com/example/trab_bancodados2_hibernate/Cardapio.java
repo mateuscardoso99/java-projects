13bbecf89abc5a02d902d34c4d123aec340e8abd
Mateus Cardoso <mat358591@gmail.com>
2024-07-14 10:13:37 -0300

    Create Cardapio.java

---
+++ trab-bancodados2-hibernate/src/main/java/com/example/trab_bancodados2_hibernate/Cardapio.java
@@ -0,0 +1,91 @@
+package com.example.trab_bancodados2_hibernate;
+
+import org.jetbrains.annotations.NotNull;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cardapio")
+public class Cardapio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(length = 50)
+    private String descricao;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurante_id", nullable = false) 
+    private Restaurante restaurante;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Restaurante getRestaurante() {
+        return restaurante;
+    }
+
+    public void setRestaurante(Restaurante restaurante) {
+        this.restaurante = restaurante;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+        result = prime * result + ((restaurante == null) ? 0 : restaurante.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cardapio other = (Cardapio) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (descricao == null) {
+            if (other.descricao != null)
+                return false;
+        } else if (!descricao.equals(other.descricao))
+            return false;
+        if (restaurante == null) {
+            if (other.restaurante != null)
+                return false;
+        } else if (!restaurante.equals(other.restaurante))
+            return false;
+        return true;
+    }
+
+    
+}
