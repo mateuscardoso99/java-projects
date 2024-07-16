c09a9541af9dc8f797e21f7afcf9e2d86f3c45d3
Mateus Cardoso <mat358591@gmail.com>
2024-07-16 16:43:08 -0300

    Update TextArea.java

--- padroes_projeto/memento/TextArea.java
+++ padroes_projeto/memento/TextArea.java
@@ -9,14 +9,19 @@ public class TextArea {
         return text;
     }
 
+    //salva no histórico os valores digitados no input
     public Memento takeSnapshot() {
         return new Memento(this.text);
     }
 
+    //quando der CTRL-Z então seta no input o valor do objeto memento mais recente dentro do histórico (que está no topo da fila)
     public void restore(Memento memento) {
         this.text = memento != null ? memento.getSavedText() : null;
     }
 
+    /*
+    o memento salva os dados que são importantes de se armazenar no histórico, nesse exemplo é o valor digitado no input
+    */
     public static class Memento {
         private final String text;
 
