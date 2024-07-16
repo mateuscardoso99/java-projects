31cc2728d1851b6c39b63001b755505174fc91b3
Mateus Cardoso <mat358591@gmail.com>
2024-07-16 16:32:51 -0300

    Update Main.java

--- padroes_projeto/memento/Main.java
+++ padroes_projeto/memento/Main.java
@@ -1,3 +1,15 @@
+/*
+
+O estado de um objeto pode ser definido como os valores de suas
+propriedades ou atributos em qualquer ponto do tempo. 
+– O Padrão Memento é útil para projetar um mecanismo para capturar e
+armazenar o estado de um objeto para que, posteriormente, quando
+necessário, o objeto pode ser colocado de volta a este estado (anterior)
+
+o padrão memento sugere também delegar a criação desses objetos que guardam os estados ao proprietário desse estado
+
+*/
+
 public class MainApp {
     public static void main(String[] args) {
 
