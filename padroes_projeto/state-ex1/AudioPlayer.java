293487e56cbb4ae20b122d9e5a70e50d4c479462
Mateus Cardoso <mat358591@gmail.com>
2024-07-16 16:01:48 -0300

    Update AudioPlayer.java

--- padroes_projeto/state-ex1/AudioPlayer.java
+++ padroes_projeto/state-ex1/AudioPlayer.java
@@ -1,3 +1,13 @@
+/*
+O estado de um objeto pode ser definido como sua condição exata em
+qualquer ponto do tempo, dependendo dos valores de suas
+propriedades ou atributos. 
+
+O padrão State é útil na concepção de uma estrutura eficiente onde
+podem existir muitos estados diferentes e exibir comportamentos
+diferentes dependendo do estado em que se encontra.
+*/
+
 public class AudioPlayer {
 
     private PlayerState estadoAtual = new EstadoParado();
