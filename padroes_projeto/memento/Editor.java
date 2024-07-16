56437d04778164e453671a4a2103366871626bfc
Mateus Cardoso <mat358591@gmail.com>
2024-07-16 16:23:09 -0300

    Create Editor.java

---
+++ padroes_projeto/memento/Editor.java
@@ -0,0 +1,31 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Editor {
+
+    private final TextArea textArea;
+    private final Deque<Memento> stateHistory; //deque é uma fila
+
+    public Editor() {
+        textArea = new TextArea();
+        stateHistory = new LinkedList<>();
+    }
+
+    public void write(String text) {
+        textArea.set(text);
+        stateHistory.offer(textArea.takeSnapshot());
+    }
+
+    public void undo() {
+        if (stateHistory.isEmpty()) {
+            return;
+        }
+        stateHistory.pollLast();
+        textArea.restore(stateHistory.peekLast());
+    }
+
+    public void printText() {
+        System.out.println(textArea.getText());
+    }
+
+}
