f6f5e6c727412536db4606ca0f5ae1e9f82430ef
Mateus Cardoso <mat358591@gmail.com>
2024-07-16 16:40:02 -0300

    Update Editor.java

--- padroes_projeto/memento/Editor.java
+++ padroes_projeto/memento/Editor.java
@@ -11,11 +11,18 @@ public class Editor {
         stateHistory = new LinkedList<>();
     }
 
+    /*
+        cada vez que o usuário digita algo no input é salvo na fila um snapshot (histórico)
+    */
     public void write(String text) {
         textArea.set(text);
         stateHistory.offer(textArea.takeSnapshot());
     }
 
+    /*
+    quando o usuário digitar CTRL-Z busca no histórico e traz o mais recente, nesse caso o do topo da fila
+    depois remove da fila esse registro do histórico e seta no input o valor dele. fazendo o efeito do CTRL-Z
+    */
     public void undo() {
         if (stateHistory.isEmpty()) {
             return;
