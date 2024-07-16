11dc0e79cec78ea21fec97c4db9cff9fa1e6b334
Mateus Cardoso <mat358591@gmail.com>
2024-07-16 16:10:08 -0300

    Update Memento.java

--- padroes_projeto/memento-ex1/Memento.java
+++ padroes_projeto/memento-ex1/Memento.java
@@ -2,6 +2,16 @@ package br.ufsm.csi.pp.exercicio6_1;
 
 import lombok.*;
 
+/*
+
+Implementar o padrão memento para o exemplo do Editor apresentado durante a aula. 
+• O editor deverá implementar um método onKeyPressed(KeyEvent k) que permite editar o texto contido neste
+• Quando o usuário pressionar espaço ou enter deverá ser criado um novo memento, desde que este não contenha o mesmo estado do memento anterior. 
+• Caso o usuário pressione Ctrl+z será desfeita a última edição. 
+• Caso o usuário pressione Ctrl+r será refeita a última edição desfeita, desde que nenhuma outra edição tenha sido feita neste tempo.
+
+*/
+
 @Data
 @Builder
 @AllArgsConstructor
