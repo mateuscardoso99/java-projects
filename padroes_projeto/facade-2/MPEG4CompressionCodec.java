774d309851f3a8baa56e1b4fb2dabf5f7efc3bb6
Mateus Cardoso <mat358591@gmail.com>
2024-07-21 14:34:55 -0300

    Add files via upload

---
+++ padroes_projeto/facade-2/MPEG4CompressionCodec.java
@@ -0,0 +1,6 @@
+package com.example;
+
+public class MPEG4CompressionCodec implements Codec {
+    public String type = "mp4";
+
+}
---
+++ padroes_projeto/facade-2/OggCompressionCodec.java
@@ -0,0 +1,5 @@
+package com.example;
+
+public class OggCompressionCodec implements Codec {
+    public String type = "ogg";
+}
