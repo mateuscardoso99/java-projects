f18ccecb9cb408d419435f48909be8517c4506bb
Mateus Cardoso <mat358591@gmail.com>
2024-07-23 10:08:51 -0300

    Create CommandHistory.java

---
+++ padroes_projeto/command-editor-texto/CommandHistory.java
@@ -0,0 +1,13 @@
+public class CommandHistory {
+    private Stack<Command> history = new Stack<>();
+
+    public void push(Command c) {
+        history.push(c);
+    }
+
+    public Command pop() {
+        return history.pop();
+    }
+
+    public boolean isEmpty() { return history.isEmpty(); }
+}
