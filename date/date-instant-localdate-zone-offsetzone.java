dfe71026ed166bf1a88f1d167116a427a71e0428
Mateus Cardoso <mat358591@gmail.com>
2024-07-26 10:44:38 -0300

    Update date-instant-localdate-zone-offsetzone.java

--- date/date-instant-localdate-zone-offsetzone.java
+++ date/date-instant-localdate-zone-offsetzone.java
@@ -109,6 +109,7 @@ significado dos símbolos no pattern
 ‘z’: fuso horário abreviação (ex: “Pacific Standard Time; PST”)
 ‘Z’: fuso horário offset (deslocamento) (e.g. “-0800”) ex: brasília será -03:00 // Este formata o offset com base no número de letras do padrão. Uma, duas ou três (ZZZ por exemplo) letras produzem a hora e o minuto, sem dois pontos, como '+0130'
 'X': semelhante ao 'Z'
+'XXX': exibe o offset nesse formtato: '-03:00'
  
 'V': time-zone ID, ex: America/Los_Angeles; Z; -08:30
 DateTimeFormatter zdtFormatter = DateTimeFormatter.ofPattern("uuuuMMdd HH:mm:ss.SSSSSS VV ZZZZZ",Locale.ENGLISH); 
@@ -121,7 +122,7 @@ exemplos:
 //dd-MMM-yyyy	  ->   “14-Jul-2018”
 //dd/MM/yyyy	 ->   “14/07/2018”
 //E, MMM dd yyyy   ->  	“Sat, Jul 14 2018”
-//h:mm a	 ->   “12:08 PM”
+//h:mm a	 ->   “12:08 PM”, 'a' mostra 'AM' ou 'PM'
 //EEEE, MMM dd, yyyy HH:mm:ss a	  ->   “Saturday, Jul 14, 2018 14:31:06 PM”
 //yyyy-MM-dd'T'HH:mm:ssZ	->   “2018-07-14T14:31:30+0530”
 //hh 'o''clock' a, zzzz	   ->   “12 o’clock PM, Pacific Daylight Time”
@@ -136,7 +137,8 @@ System.out.println(new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz").format(
 
 
 ZONE: se refere ao nome do continente, país, cidade
-OffsetDateTime: é uma representação imutável de uma data-hora com um deslocamento de UTC/Greenwich no sistema de calendário ISO-8601, como 2007-12-03T10:15:30+01:00 . Em outras palavras, ele armazena todos os campos de data e hora, com uma precisão de nanossegundos, bem como o deslocamento de GMT/UTC .
+OffsetDateTime: é uma representação imutável de uma data-hora com um deslocamento de UTC/Greenwich no sistema de calendário ISO-8601, como '2007-12-03T10:15:30+01:00' . 
+Em outras palavras, ele armazena todos os campos de data e hora, com uma precisão de nanossegundos, bem como o deslocamento de GMT/UTC .
 //Vamos obter o  OffsetDateTime  atual com um deslocamento de duas horas de GMT/UTC:
 ZoneOffset zoneOffSet= ZoneOffset.of("+02:00");
 OffsetDateTime offsetDateTime = OffsetDateTime.now(zoneOffSet);
