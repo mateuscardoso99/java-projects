7b3f014eb3fb562d237080d56dc6349e71c28151
Mateus Cardoso <mat358591@gmail.com>
2024-10-11 18:01:42 -0300

    Create Alice.java

---
+++ topicos-seguranca/cripto-assimetrica-professor/src/main/java/com/example/Alice.java
@@ -0,0 +1,60 @@
+package br.ufsm.poli.csi.cripto.assimetrico;
+
+import lombok.SneakyThrows;
+
+import javax.crypto.*;
+import javax.swing.*;
+import java.io.*;
+import java.net.Socket;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+
+public class Alice {
+
+    @SneakyThrows
+    public static void main(String[] args) throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+
+        JFileChooser fc = new JFileChooser("");
+        System.out.println("Selecionando arquivo");
+        if (fc.showDialog(new JFrame(), "OK") == JFileChooser.APPROVE_OPTION) {
+            File f = fc.getSelectedFile();
+            FileInputStream fin = new FileInputStream(f);
+            byte[] bArray = new byte[(int) fin.getChannel().size()];
+            fin.read(bArray);
+            System.out.println("Arquivo selecionado");
+
+            //cria a chave de sessão
+            KeyGenerator kg = KeyGenerator.getInstance("AES");
+            kg.init(256);
+            SecretKey chaveSessao = kg.generateKey();
+
+            //inicia conexao com o Bob e recebe a sua chave pública
+            Socket s = new Socket("localhost", 5555);
+            ObjectInputStream oin = new ObjectInputStream(s.getInputStream());
+            ObjetoTroca objetoChave = (ObjetoTroca) oin.readObject();
+
+            //criptografa o arquivo com a chave publica do Bob
+            Cipher cipherRSA = Cipher.getInstance("RSA");
+            Cipher cipherAES = Cipher.getInstance("AES");
+            cipherRSA.init(Cipher.ENCRYPT_MODE, objetoChave.getKey());
+            cipherAES.init(Cipher.ENCRYPT_MODE, chaveSessao);
+            byte[] chaveSessaoCifrada = cipherRSA.doFinal(chaveSessao.getEncoded());
+            byte[] textoCifrado = cipherAES.doFinal(bArray);
+
+            ObjetoTroca objetoTroca = new ObjetoTroca();
+            objetoTroca.setArquivoCriptografado(textoCifrado);
+            objetoTroca.setNomeArquivo(f.getName());
+            objetoTroca.setChaveSessao(chaveSessaoCifrada);
+
+            //envia arquivo
+            ObjectOutputStream oout = new ObjectOutputStream(s.getOutputStream());
+            oout.writeObject(objetoTroca);
+            s.close();
+
+
+        }
+    }
+
+}
